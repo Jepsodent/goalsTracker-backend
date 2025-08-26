ed9232fb187d27ea6c2148821f27a2d2f1453c64
Jefferson <jefferson.swanto@gmail.com>
2025-08-26 19:55:17 +0700

    Add missing user route in server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-
+import cookieParser from "cookie-parser";
+import userRoute from "./route/userRoute.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 await connectDB();
 
@@ -15,5 +17,7 @@ await connectDB();
 app.get('/', (req, res) => {
     res.json({success:true , message: "API IS WORKING"});
 })
+app.use('/api/user', userRoute);
+
 
 app.listen(PORT, () => console.log(`Server Running at http://localhost:${PORT}`));
